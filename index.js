e7421903c10955063f1336046184203b73644cc8
Duy Hien <duyhienn.2012@gmail.com>
2023-11-16 10:10:34 +0700

    change route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 
-app.get('/contact', function (req, res) {
+app.get('/about', function (req, res) {
     res.send("Hello node")
 })
 app.listen(3000, function () {
